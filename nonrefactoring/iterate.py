a6cc0e7c5904fa78c3e4ef8e820d272aeb2f5741
shikham-8 <shikhamody1@gmail.com>
2022-06-02 21:36:37 -0700

    file added

--- nonrefactoring/iterate.py
+++ nonrefactoring/iterate.py
@@ -1,14 +1,17 @@
-import os, imp
+import os
+import imp
 directory = 'examples'
 
-for filename in os.listdir(directory):
+
+def iterateOverFile(filename):
     if filename.endswith(".py"):
         modulename = filename[:-3]
         print(modulename)
         print(directory+os.sep+modulename)
-        foo = imp.load_source('module', directory+os.sep+filename)
-        for name, val in foo.__dict__.iteritems(): # iterate through every module's attributes
+        foo = imp.load_source('module', filename)
+        for name, val in foo.__dict__.items():  # iterate through every module's attributes
             if callable(val):                      # check if callable (normally functions)
                 print(val)                              # call it
-    else:
-        continue
+
+
+# iterateOverFile("examples/cover4.py")
