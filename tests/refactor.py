64288a1c0c3fa5ba3ecfd12c20c6204683c27ba5
shikham-8 <shikhamody1@gmail.com>
2022-06-05 23:30:17 -0700

    refactor update

--- tests/refactor.py
+++ tests/refactor.py
@@ -2,8 +2,7 @@ from typing import List
 
 
 def increment_list(a: List[int]) -> None:
-    a_length = len(a)
-    for i in range(a_length):
-        a.append(a[i] + 1)
-    for i in range(a_length):
-        a.pop(0)
+    i = 0
+    while i < len(a) - 1:
+        a[i] += 1
+        i += 1
